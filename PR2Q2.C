74083588d0623a466900ab13565a276747c4bca3
Disha-vaghasiya <149076733+Disha-vaghasiya@users.noreply.github.com>
2023-12-25 14:29:00 +0530

    Update PR2Q2.C

--- PR2Q2.C
+++ PR2Q2.C
@@ -2,12 +2,18 @@
 #include<conio.h>
 void main(){
 int units;
-float amount;
+float amount,total;
 clrscr();
    printf("enter units...:-  ");
    scanf("%d",&units);
 
-   if(units<50) amount=units*0.50;
-   else if(units<150) amount=50*0.50+0.75;
+   if{(units<50) amount=units*0.50;}
+   else if{(units<150) amount=25 + ((units-50)*0.75;)}
+   else if{(units<=250) amount=100 + ((units-150)*1.20;)}
+  else {amount=220 + ((units-250)*1.50)}
+
+total=amount*20/100;
+printf("Electricity bill is :%f",total+amount)
+  }
 getch();
 }
